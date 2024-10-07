8b7e16e83e6c8d66f7d9f2bcd3b0900aeb481a39
Frédéric Pilon <fredy_gold5@hotmail.com>
2024-10-07 16:48:33 -0400

    Work on results

--- _scripts/build_export_csv.js
+++ _scripts/build_export_csv.js
@@ -1,7 +1,28 @@
 const fs = require('node:fs');
 
-build_export_csv();
-function build_export_csv(){
+// Make it possible to call both functions from the package.json
+for(var i=0;i<process.argv.length;i++){
+    switch(process.argv[i]){
+        case 'daily':
+            build_export_csv(1,process.argv[i]);
+            return false;
+            break;
+        case 'weekly':
+            build_export_csv(7,process.argv[i]);
+            return false;
+            break;
+        case 'monthly':
+            build_export_csv(365/12,process.argv[i]);
+            return false;
+            break;
+        case 'endresult':
+            build_export_csv(99999,process.argv[i]);
+            return false;
+            break;
+    }
+}
+module.exports.build_export_csv = build_export_csv
+function build_export_csv(daysBetweenPicks=1,fileSuffix=''){
     let possibledays = [];
     let csvExport = [];
     let directory = __dirname+'/../data/elo/';
@@ -41,31 +62,45 @@ function build_export_csv(){
                 }
                 let dailyElo = {};
                 let currentLookedForDay = Date.parse(possibledays[0])/1000;
-                elocontent.forEach(elo=>{
-                    if(elo.time>currentLookedForDay){
-                        let date = new Date(elo.time*1000);
-                        let month = ('0' + (date.getMonth()+1)).slice(-2)
-                        let day = ('0' + (date.getDate())).slice(-2)
-                        dailyElo[date.getFullYear()+'-'+month+'-'+day] = elo.score;
-                        currentLookedForDay+=(60*60*24);
+                if(daysBetweenPicks != 99999){
+                    elocontent.forEach(elo=>{
+                        if(elo.time>currentLookedForDay){
+                            let date = new Date(elo.time*1000);
+                            let month = ('0' + (date.getMonth()+1)).slice(-2)
+                            let day = ('0' + (date.getDate())).slice(-2)
+                            dailyElo[date.getFullYear()+'-'+month+'-'+day] = elo.score;
+                            currentLookedForDay+=(60*60*24*daysBetweenPicks);
+                        }
+                    })
+                    let previousResult = 1000;
+                    if(daysBetweenPicks == 1){
+                        possibledays.forEach(day=>{
+                            if(dailyElo[day]){
+                                previousResult = dailyElo[day];
+                            }else{
+                                dailyElo[day] = previousResult;
+                            }
+                        })
                     }
-                })
-                let previousResult = 1000;
-                possibledays.forEach(day=>{
-                    if(dailyElo[day]){
-                        previousResult = dailyElo[day];
-                    }else{
-                        dailyElo[day] = previousResult;
-                    }
-                })
-                let toCleanup = dailyElo;
-                dailyElo = Object.keys(toCleanup).sort().reduce(
-                    (obj, key) => { 
-                        obj[key] = toCleanup[key]; 
-                        return obj;
-                    }, 
-                    {}
-                );
+                    let toCleanup = dailyElo;
+                    dailyElo = Object.keys(toCleanup).sort().reduce(
+                        (obj, key) => { 
+                            obj[key] = toCleanup[key]; 
+                            return obj;
+                        }, 
+                        {}
+                    );
+                }else{
+                    let currentScore = 0;
+                    let currentTime = 0;
+                    elocontent.forEach(elo=>{
+                        if(currentTime<elo.time){
+                            currentScore = elo.score;
+                            currentTime = elo.time;
+                        }
+                    })
+                    dailyElo['Current score'] = currentScore;
+                }
                 //console.log(songcontent);
                 if(songcontent){
                     addToExport = {...songcontent,...dailyElo};
@@ -75,10 +110,17 @@ function build_export_csv(){
                 csvExport.push(addToExport);
             }
         }
-        csvExport.sort(function(a,b){
-            return b[possibledays[possibledays.length-1]]-a[possibledays[possibledays.length-1]]
-        })
-        fs.writeFileSync(__dirname+'/../data/full-results.json',JSON.stringify(csvExport));
+        if(daysBetweenPicks != 99999){
+            csvExport.sort(function(a,b){
+                return b[possibledays[possibledays.length-1]]-a[possibledays[possibledays.length-1]]
+            })
+        }else{
+            csvExport.sort(function(a,b){
+                return b["Current score"]-a["Current score"]
+            })
+        }
+        //console.log(JSON.stringify(csvExport))
+        fs.writeFileSync(__dirname+'/../data/_results/results-'+fileSuffix+'.json',JSON.stringify(csvExport));
 
         var json = csvExport
         var fields = Object.keys(json[0])
@@ -90,7 +132,7 @@ function build_export_csv(){
         })
         csv.unshift(fields.join(',')) // add header column
         csv = csv.join('\r\n');
-        fs.writeFileSync(__dirname+'/../data/full-results.csv',csv);
+        fs.writeFileSync(__dirname+'/../data/_results/results-'+fileSuffix+'.csv',csv);
 
         console.log('DONE! Check the data folder for results.')
     })
