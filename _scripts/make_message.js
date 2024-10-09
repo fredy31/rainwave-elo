d49acc52b038ed65a82198a4f8dbacdee4b75f17
Frédéric Pilon <fredy_gold5@hotmail.com>
2024-10-09 19:36:10 -0400

    Work

--- _scripts/make_message.js
+++ _scripts/make_message.js
@@ -3,6 +3,9 @@ const fs = require('node:fs');
 make_message();
 function make_message(){
     let results = '';
+    let quickMessage = '';
+
+    // Results
     try{
         results = fs.readFileSync(__dirname+'/../data/_results/results-endresult.json');
         results = JSON.parse(results);
@@ -10,9 +13,49 @@ function make_message(){
         console.error(err);
     }
 
-    let quickMessage = '';
+    quickMessage += '### Best songs per score ###\n\r';
     for(let i=0;i<5;i++){
         quickMessage += results[i]["title"] + ' of '+results[i]["album"] + ' ('+results[i]['Current score']+')\n\r';
     }
-    fs.writeFileSync(__dirname+'/../data/_results/quickrank-message.txt',quickMessage);
+
+    // Outliers
+    try{
+        results = fs.readFileSync(__dirname+'/../data/_results/biggest_differences_score.json');
+        results = JSON.parse(results);
+    }catch(err){
+        console.error(err);
+    }
+
+    quickMessage += '### Biggest differences ###\n\r';
+    for(let i=0;i<5;i++){
+        quickMessage += results[i]["title"] + ' of '+results[i]["album"] + ' (Vote:'+results[i]['rating']+'; ELO:'+results[i]['rankscore']+'%; Difference of '+results[i]['scoreDiff']+')\n\r';
+    }
+
+    // Best Albums
+    try{
+        results = fs.readFileSync(__dirname+'/../data/_results/averages_album.json');
+        results = JSON.parse(results);
+    }catch(err){
+        console.error(err);
+    }
+
+    quickMessage += '### Best Albums ###\n\r';
+    for(let i=0;i<5;i++){
+        quickMessage += results[i]["elementName"] + ' ('+results[i]['score']+') ['+results[i]['nbOfSongs']+' songs]\n\r';
+    }
+
+    // Best Artists
+    try{
+        results = fs.readFileSync(__dirname+'/../data/_results/averages_artist.json');
+        results = JSON.parse(results);
+    }catch(err){
+        console.error(err);
+    }
+
+    quickMessage += '### Best Artists ###\n\r';
+    for(let i=0;i<5;i++){
+        quickMessage += results[i]["elementName"] + ' ('+results[i]['score']+') ['+results[i]['nbOfSongs']+' songs]\n\r';
+    }
+
+    fs.writeFileSync(__dirname+'/../data/_results/_quickrank-message.md',quickMessage);
 }
---
+++ data/_results/_quickrank-message.md
@@ -0,0 +1,25 @@
+### Best songs per score ###
+Main Theme of Bubble Bobble (1144)
+Stage 2 of Marble Madness (1135)
+Sepulchre Soirée of Crypt of the NecroDancer: OverClocked (1126)
+Whoa I'm In Space Cuba (BONUS CHIP MIX) of Mighty Switch Force! (1117)
+Be Kind, Rewind of Candy Corn II: The Sequeling (1117)
+### Biggest differences ###
+Big Tree of Fire 'N Ice (Vote:3.1783783; ELO:99.72871721999742%; Difference of 36.16115121999742)
+Title of Green Beret (Vote:3.2385964; ELO:99.88696550833225%; Difference of 35.11503750833225)
+bra reloCaTing of Cobra Triangle (Vote:3.3247058; ELO:98.92455755070404%; Difference of 32.430441550704046)
+Thing on a Spring of Thing on a Spring (Vote:3.3292308; ELO:98.96977134737114%; Difference of 32.38515534737114)
+Player 1 Theme of Tecmo Bowl (Vote:3.3185184; ELO:98.73401369332127%; Difference of 32.363645693321274)
+### Best Albums ###
+Rhythm Gunner (1117) [1 songs]
+Dragon Souseiki (1117) [1 songs]
+Beatmania IIDX 7th Style (1108) [1 songs]
+Imploder (1099) [1 songs]
+8 Bit of Zelda: Ocarina of Time (1099) [1 songs]
+### Best Artists ###
+Tadashi Kimijima (1144) [1 songs]
+Hal Canon, Brad Fuller, Steve Hayes, David Wise (1135) [1 songs]
+Takahiro Matsuki (1117) [1 songs]
+Xaleph, ZackParrish, Audiomint (1108) [1 songs]
+chumble spuzz (1099) [1 songs]
+
